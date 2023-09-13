3709d4eb039d6514a2fcb76ecad6439ac1cc29a5
hwayoung <lweyine.sm@gmail.com>
2023-09-13 22:39:12 +0900

    Chore: 버튼 클릭 이벤트 함수 정의

--- mohamco/src/components/NavBar.tsx
+++ mohamco/src/components/NavBar.tsx
@@ -4,7 +4,21 @@ import '../styles/NavBar.css';
 const NavBar = () => {
 
   const menus = ["자유게시판", "질문게시판", "모집게시판", "정보게시판", "스터디", "프로젝트"];
-  const menuList = menus.map((menu) => (<li><a href="#" className="nav-link px-2"><span className="menu">{menu}</span></a></li>))
+  const menu_link = ["#", "#", "#", "#", "#", "#"]; //메뉴 링크
+
+  function menuClick() {      //메뉴 클릭 이벤트
+    console.log("test");
+  }
+
+  function loginBtnClick() {  //로그인 버튼 클릭 이벤트
+
+  }
+
+  function signUpBtnClick() { //회원가입 버튼 클릭 이벤트
+
+  }
+
+  const menuList = menus.map((menu, menu_link) => (<li><a href="#" onClick={menuClick} className="nav-link px-2"><span className="menu">{menu}</span></a></li>))
 
 return(
 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -19,8 +33,8 @@ return(
       </ul>
 
       <div className="col-md-3 text-end mx-4">
-        <button type="button" className="loginbtn btn btn-outline-secondary me-2">로그인</button>
-        <button type="button" className="signupbtn btn btn-primary">회원가입</button>
+        <button type="button" onClick={loginBtnClick} className="loginbtn btn btn-outline-secondary me-2">로그인</button>
+        <button type="button" onClick={signUpBtnClick} className="signupbtn btn btn-primary">회원가입</button>
       </div>
 </header>
 );
