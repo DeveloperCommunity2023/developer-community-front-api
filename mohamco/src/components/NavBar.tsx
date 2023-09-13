4028a03195700cca25f5c1d4fd22ee68ecc4f5e1
hwayoung <lweyine.sm@gmail.com>
2023-09-13 21:27:08 +0900

    Design NavBar css 적용

--- mohamco/src/components/NavBar.tsx
+++ mohamco/src/components/NavBar.tsx
@@ -1,16 +1,16 @@
 
-import "../styles/NavBar.css";
+import '../styles/NavBar.css';
 
 const NavBar = () => {
 
   const menus = ["자유게시판", "질문게시판", "모집게시판", "정보게시판", "스터디", "프로젝트"];
-  const menuList = menus.map((menu) => (<li><a href="#" className="nav-link px-2">{menu}</a></li>))
+  const menuList = menus.map((menu) => (<li><a href="#" className="nav-link px-2"><span className="menu">{menu}</span></a></li>))
 
 return(
 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <div className="col-md-3 mb-2 mb-md-0">      
         <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none justify-content-center">
-          <span className="logo">모함코</span>
+          <span className="logo mx-4">모함코</span>
         </a>
       </div>
 
@@ -18,9 +18,9 @@ return(
         {menuList}
       </ul>
 
-      <div className="col-md-3 text-end">
-        <button type="button" className="btn btn-outline-primary me-2">로그인</button>
-        <button type="button" className="btn btn-primary">회원가입</button>
+      <div className="col-md-3 text-end mx-4">
+        <button type="button" className="loginbtn btn btn-outline-secondary me-2">로그인</button>
+        <button type="button" className="signupbtn btn btn-primary">회원가입</button>
       </div>
 </header>
 );
