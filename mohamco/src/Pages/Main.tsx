5f494d3f1fefcb690e1158ca446c1401c9b3c1dd
박수진 <cssj@5works.co.kr>
2023-09-19 17:16:49 +0900

    Feat: 메인 틀 일부

--- mohamco/src/Pages/Main.tsx
+++ mohamco/src/Pages/Main.tsx
@@ -2,14 +2,60 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/NavBar";
 
 export function Main() {
- 
+    const list = ["test1sdfgsdfgsdff","test2sdfgsdfgsdfg","test3asdfasdfa","test4asdfsdf"];
+    const test = list.map((x) => (
+      <div>
+        <ul className="px-2 py-1/3 divide-y divide-gray-500/30 dark:divide-gray-500/70 list-unstyled">
+          <li><div className="mb-2 flex">{x}</div></li>
+        </ul>
+      </div>
+    ));
+
     return (
         <main>
-            <Navbar></Navbar>
+          <Navbar></Navbar>
+
+          <div className="d-flex justify-content-center my-8 flex flex-col gap-x-10 gap-y-10 md:flex-row">
+            <div className="w-25 mx-5">
+              <div className="border-bottom p-2 bg-light rounded">
+                  <a className="relative mb-2 text-gray-900 no-underline dark:text-gray-100">
+                    asd
+                  </a>
+              </div>
+              {test}
+            </div>
+            <div className="w-25 mx-5">
+              <div className="border-bottom p-2 bg-light rounded">
+                <a className="relative mb-2 text-gray-900 no-underline dark:text-gray-100">
+                  asd
+                </a>
+              </div>
+              {test}
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-center my-8 flex flex-col gap-x-10 gap-y-10 md:flex-row">
+            <div className="w-25 mx-5">
+              <div className="border-bottom p-2 bg-light rounded">
+                  <a className="relative mb-2 text-gray-900 no-underline dark:text-gray-100">
+                    asd
+                  </a>
+              </div>
+              {test}
+            </div>
+            <div className="w-25 mx-5">
+              <div className="border-bottom p-2 bg-light rounded">
+                <a className="relative mb-2 text-gray-900 no-underline dark:text-gray-100">
+                  asd
+                </a>
+              </div>
+              {test}
+            </div>
+          </div>
         </main>
-        
-    
+
+
     )
-    
+
 
 }
