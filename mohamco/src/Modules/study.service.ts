2e5830898c50bd0522dce9e9d75ac0722e374f2b
박수진 <cssj@5works.co.kr>
2023-11-10 14:41:31 +0900

    Feat: 스터디 메인 스터디 카드 띄우기, 페이지네이션 버튼 추가

--- mohamco/src/Modules/study.service.ts
+++ mohamco/src/Modules/study.service.ts
@@ -2,5 +2,5 @@ import instance from "../instance";
 
 export async function getStudy() {
   const data = await (await instance.get(`api/v1/study/main`)).data;
-  return data;
+  return data.data.studyList;
 }
--- mohamco/src/Pages/Study.tsx
+++ mohamco/src/Pages/Study.tsx
@@ -1,13 +1,23 @@
 import NavBar from "../components/NavBar";
 import { BsFillPeopleFill } from "react-icons/bs";
-import { Avatar, Card, Select, Input, Button } from "antd";
+import {
+  Avatar,
+  Card,
+  Select,
+  Input,
+  Button,
+  Row,
+  Col,
+  Pagination,
+} from "antd";
 import { useEffect, useState } from "react";
-import { StudyDto } from "../Models/stydy,dto";
+import { StudyList } from "../Models/stydy,dto";
 import { getStudy } from "../Modules/study.service";
+import { Footer } from "antd/es/layout/layout";
 
 export function Study() {
   const { Meta } = Card;
-  const [data, setData] = useState<StudyDto>();
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
     init();
@@ -18,53 +28,85 @@ export function Study() {
       setData(res);
     });
   };
-  console.log("ss", data);
-  console.log("ss1", data?.studyList);
+
+  console.log("res", data);
   return (
     <>
       <NavBar></NavBar>
-      <div>
-        <div>
-          <h4>스터디 모임</h4>
+      <div
+        style={{
+          padding: "10px",
+        }}
+      >
+        <h4>
+          <strong>스터디 모임</strong>
+        </h4>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
           <Select defaultValue={"최신순"} value={"최신순"} placeholder="최신순">
             <Select.Option value="최신순">최신순</Select.Option>
           </Select>
-        </div>
-        <div style={{ display: "inline-flex", justifyContent: "right" }}>
-          <Select defaultValue={"전체"} value={"전체"} placeholder="전체">
-            <Select.Option value="전체">전체</Select.Option>
-          </Select>
-          <Input />
-          <Button type="primary">검색</Button>
-        </div>
-      </div>
-      {/* {data && data.studyList.map((x, idx) => {
-          <div>{idx}</div>; */}
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Card
-          style={{ width: 300 }}
-          cover={
-            <img
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-          }
-        >
-          <Meta
-            avatar={
-              <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
-            }
-            // title={x.studyName}
-            // description={x.studyDesc}
-          />
-          <div>여기에 태그 넣고</div>
-          <div style={{ textAlign: "right" }}>
-            <BsFillPeopleFill></BsFillPeopleFill>
-            100
+
+          <div style={{ display: "flex", position: "absolute", right: 100 }}>
+            <Select defaultValue={"전체"} value={"전체"} placeholder="전체">
+              <Select.Option value="전체">전체</Select.Option>
+            </Select>
+            <Input />
+            <Button type="primary">검색</Button>
           </div>
-        </Card>
+        </div>
       </div>
-      ;{/* })} */}
+      <Row>
+        {data &&
+          data.map((x, idx) => (
+            <Col span={6}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: "20px",
+                }}
+              >
+                <Card
+                  style={{
+                    width: 300,
+                  }}
+                  cover={
+                    <img
+                      alt="example"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                  }
+                >
+                  <Meta
+                    avatar={
+                      <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+                    }
+                    title={x.studyName}
+                    description={x.studyDesc}
+                  />
+                  <div>여기에 태그 넣고</div>
+                  <div style={{ textAlign: "right" }}>
+                    <BsFillPeopleFill></BsFillPeopleFill>
+                    {x.studyCount}
+                  </div>
+                </Card>
+              </div>
+            </Col>
+          ))}
+      </Row>
+
+      <div
+        style={{ display: "flex", justifyContent: "center", marginBottom: 10 }}
+      >
+        <Pagination defaultCurrent={1} />
+        <Button style={{ position: "absolute", right: 50 }} type="primary">
+          생성하기
+        </Button>
+      </div>
     </>
   );
 }
